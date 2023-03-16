c6b40c2ae961792bf1d2393e22d95b7d3f57b566
Narmina777 <127624119+Narmina777@users.noreply.github.com>
2023-03-16 18:58:07 +0400

    n777

---
+++ Task.1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[2];
+            Employee firstEmployee = new Employee()
+            {
+                Name = "A",
+                Department = "Developers",
+
+
+            };
+            //firstEmployee.Name = "A";
+            //employees[0] = firstEmployee;
+
+
+            Employee secondEmployee = new Employee()
+            {
+                Name = "B",
+                Department = "Developers",
+            };
+
+            //firstEmployee.Name = "B";
+            //employees[1] = secondEmployee;
+            for (int i = 0; i < employees.Length; i++) ;
+            {
+                employees[i].PrintInfo();
+            }
+
+        }
+    }
+
+
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Sound of " + Name);
+        }
+
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int ID { get; set; }
+        public string Department { get; set; }
+        public string Team { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Team of " + Name);
+
+        }
+
+       
+
+    }
+}
